704a81b6fb7e2722d3e2e18ed0b9c6484679b9ca
4o4E <869951226@qq.com>
2022-05-23 01:41:11 +0800

    添加shiny图片处理器

---
+++ src/main/kotlin/handler/face/ShinyHandler.kt
@@ -0,0 +1,40 @@
+package top.e404.skiko.handler.face
+
+import org.jetbrains.skia.Paint
+import org.jetbrains.skia.Rect
+import top.e404.skiko.Colors
+import top.e404.skiko.apt.annotation.ImageHandler
+import top.e404.skiko.frame.Frame
+import top.e404.skiko.frame.FramesHandler
+import top.e404.skiko.frame.HandleResult.Companion.result
+import top.e404.skiko.frame.common
+import top.e404.skiko.frame.handle
+import top.e404.skiko.util.*
+import top.e404.skiko.util.getJarImage
+
+@ImageHandler
+object ShinyHandler : FramesHandler {
+    private val cover = getJarImage("statistic/shiny.png")
+    private const val size = 170
+    private val faceRect = Rect.makeXYWH(157F, 114F, size.toFloat(), size.toFloat())
+    private val imgRect = Rect.makeWH(cover.width.toFloat(), cover.height.toFloat())
+    private val paint = Paint().apply {
+        color = Colors.WHITE.argb
+    }
+
+    override val name = "闪耀"
+    override val regex = Regex("(?i)闪耀|shiny")
+
+    override suspend fun handleFrames(
+        frames: MutableList<Frame>,
+        args: MutableMap<String, String>,
+    ) = frames.result {
+        common(args).handle {
+            cover.toSurface().withCanvas {
+                drawRect(imgRect, paint)
+                drawImage(cover, 0F, 0F)
+                drawImageRect(round(), faceRect)
+            }
+        }
+    }
+}

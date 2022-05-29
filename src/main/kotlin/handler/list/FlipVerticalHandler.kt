33fb97413ff210bd83666a0623158c72d6f51528
4o4E <869951226@qq.com>
2022-05-30 00:46:16 +0800

    -

--- src/main/kotlin/handler/list/FlipVerticalHandler.kt
+++ src/main/kotlin/handler/list/FlipVerticalHandler.kt
@@ -4,8 +4,11 @@ import org.jetbrains.skia.Image
 import org.jetbrains.skia.Matrix33
 import org.jetbrains.skia.Surface
 import top.e404.skiko.apt.annotation.ImageHandler
-import top.e404.skiko.frame.*
+import top.e404.skiko.frame.Frame
+import top.e404.skiko.frame.FramesHandler
 import top.e404.skiko.frame.HandleResult.Companion.result
+import top.e404.skiko.frame.common
+import top.e404.skiko.frame.handle
 import top.e404.skiko.util.withCanvas
 
 /**

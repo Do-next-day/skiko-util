85afbc09b81fbfaad4ba0757b21e3ccb41ae98de
4o4E <869951226@qq.com>
2022-06-01 00:11:00 +0800

    修复bug

--- src/main/kotlin/util/util.kt
+++ src/main/kotlin/util/util.kt
@@ -136,23 +136,27 @@ val grayMatrix = ColorFilter.makeMatrix(
 fun String?.intOrPercentage(default: Int) = when {
     this == null -> default
     endsWith("%") -> -removeSuffix("%").toInt()
+    trim() == "" -> default
     else -> toInt()
 }
 
 fun String?.floatOrPercentage(default: Float) = when {
     this == null -> default
     endsWith("%") -> -removeSuffix("%").toFloat()
+    trim() == "" -> default
     else -> toFloat()
 }
 
 fun String?.doubleOrPercentage(default: Double) = when {
     this == null -> default
     endsWith("%") -> -removeSuffix("%").toDouble()
+    trim() == "" -> default
     else -> toDouble()
 }
 
 fun String?.doubleOrPercentage(default: Double? = null) = when {
     this == null -> default
     endsWith("%") -> -removeSuffix("%").toDouble()
+    trim() == "" -> default
     else -> toDouble()
 }

4292c6aca8113c1b12bf8bb36a848eb4efbb86ea
4o4E <869951226@qq.com>
2022-06-01 18:08:41 +0800

    新整fondle图片处理器

---
+++ src/main/kotlin/handler/face/FondleHandler.kt
@@ -0,0 +1,42 @@
+package top.e404.skiko.handler.face
+
+import org.jetbrains.skia.Surface
+import top.e404.skiko.apt.annotation.ImageHandler
+import top.e404.skiko.frame.*
+import top.e404.skiko.frame.HandleResult.Companion.result
+import top.e404.skiko.handler.DrawData
+import top.e404.skiko.util.getJarImage
+import top.e404.skiko.util.pmapIndexed
+import top.e404.skiko.util.round
+import top.e404.skiko.util.withCanvas
+
+/**
+ * 嫌弃
+ */
+@ImageHandler
+object FondleHandler : FramesHandler {
+    private const val w = 100
+    private const val h = 110
+    private const val count = 28
+    private val range = 0..count
+    private val bgList = range.map { getJarImage("statistic/fondle/$it.png") }
+    private val ddList = DrawData.loadFromJar("statistic/fondle/fondle.yml")
+
+    override val name = "fondle"
+    override val regex = Regex("(?i)fondle")
+
+    override suspend fun handleFrames(
+        frames: MutableList<Frame>,
+        args: MutableMap<String, String>,
+    ) = frames.handle { round() }.common(args).replenish(count, Frame::limitAsGif).result {
+        common(args).pmapIndexed { index ->
+            duration(80)
+            handle {
+                Surface.makeRasterN32Premul(w, h).withCanvas {
+                    drawImage(bgList[index], 0F, 0F)
+                    ddList[index].draw(this, image)
+                }
+            }
+        }
+    }
+}

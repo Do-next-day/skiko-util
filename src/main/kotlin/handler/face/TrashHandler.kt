55661a6de2582c2cc14302e6eecd787e2d89288b
4o4E <869951226@qq.com>
2022-06-19 23:16:00 +0800

    新增trash图片处理器

---
+++ src/main/kotlin/handler/face/TrashHandler.kt
@@ -0,0 +1,38 @@
+package top.e404.skiko.handler.face
+
+import org.jetbrains.skia.Rect
+import org.jetbrains.skia.Surface
+import top.e404.skiko.Colors
+import top.e404.skiko.apt.annotation.ImageHandler
+import top.e404.skiko.frame.*
+import top.e404.skiko.frame.HandleResult.Companion.result
+import top.e404.skiko.util.getJarImage
+import top.e404.skiko.util.round
+import top.e404.skiko.util.withCanvas
+
+/**
+ * 垃圾
+ */
+@ImageHandler
+object TrashHandler : FramesHandler {
+    private const val w = 116
+    private const val h = 118
+    private val bg by lazy { getJarImage("statistic/trash.png") }
+    private val rect = Rect.makeXYWH(41F, 30F, 68F, 68F)
+
+    override val name = "trash"
+    override val regex = Regex("(?i)trash")
+
+    override suspend fun handleFrames(
+        frames: MutableList<Frame>,
+        args: MutableMap<String, String>,
+    ) = frames.handle { round() }.common(args).result {
+        handleIndexed { index ->
+            Surface.makeRasterN32Premul(w, h).withCanvas {
+                clear(Colors.WHITE.argb)
+                drawImageRect(this@handleIndexed, rect)
+                drawImage(bg, 0F, 0F)
+            }
+        }
+    }
+}

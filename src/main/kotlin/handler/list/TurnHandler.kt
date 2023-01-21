d8d541b306d7844c832d0835fb8c451ea49055d6
4o4E <869951226@qq.com>
2023-01-22 01:14:24 +0800

    使用矩阵优化绘制

--- src/main/kotlin/handler/list/TurnHandler.kt
+++ src/main/kotlin/handler/list/TurnHandler.kt
@@ -1,11 +1,16 @@
 package top.e404.skiko.handler.list
 
+import org.jetbrains.skia.Matrix33
+import org.jetbrains.skia.Surface
 import top.e404.skiko.apt.annotation.ImageHandler
 import top.e404.skiko.frame.*
 import top.e404.skiko.frame.HandleResult.Companion.result
-import top.e404.skiko.util.rotateKeepSize
+import top.e404.skiko.util.drawImageRectNearest
+import top.e404.skiko.util.newSurface
 import top.e404.skiko.util.round
+import top.e404.skiko.util.withCanvas
 import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * 生成转动gif
@@ -18,11 +23,36 @@ object TurnHandler : FramesHandler {
         frames: MutableList<Frame>,
         args: MutableMap<String, String>,
     ): HandleResult {
-        val count = args["text"]?.toIntOrNull() ?: 10
+        val count = args["text"]?.toIntOrNull()?.coerceIn(-50, 50) ?: 10
+        val r = args.containsKey("r")
         return frames.common(args).replenish(abs(count), Frame::limitAsGif).result {
             val v = (360F / size).let { if (count < 0) -it else it }
             handleIndexed { index, image ->
-                image.round().rotateKeepSize(index * v)
+                if (r) {
+                    // 不应用圆形蒙版
+                    val size = first().run { sqrt(1.0 * image.width * image.width + image.height * image.height) }.toInt()
+                    return@handleIndexed Surface.makeRasterN32Premul(size, size).withCanvas {
+                        setMatrix(
+                            Matrix33
+                                .makeTranslate((size - image.width) / 2F, (size - image.height) / 2F)
+                                .makeConcat(Matrix33.makeTranslate(image.width / 2F, image.height / 2F))
+                                .makeConcat(Matrix33.makeRotate(index * v))
+                                .makeConcat(Matrix33.makeTranslate(image.width / -2F, image.height / -2F))
+                        )
+                        drawImageRectNearest(image)
+                    }
+                }
+                // 不应用圆形蒙版
+                val i = image.round()
+                i.newSurface().withCanvas {
+                    setMatrix(
+                        Matrix33
+                            .makeTranslate(i.width / 2F, i.height / 2F)
+                            .makeConcat(Matrix33.makeRotate(index * v))
+                            .makeConcat(Matrix33.makeTranslate(i.width / -2F, i.height / -2F))
+                    )
+                    drawImageRectNearest(i)
+                }
             }
         }
     }
